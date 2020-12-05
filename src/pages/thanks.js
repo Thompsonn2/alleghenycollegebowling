1c605e1f283e563f62f7d957cb48c31b3f3c8f2e
Nolan <thompsonn2@allegheny.edu>
2020-12-05 18:09:35 -0500

    Updated Submission Statement

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -15,9 +15,9 @@ const Thanks = () => (
         fontSize: "128px",
         color: "var(--primary-color)"
       }}/>
-      <h1>Got your message</h1>
-      <p>Thank you for getting in touch us. We will get back to you shortly.</p>
-      <Link to="/" className="button"><RiArrowLeftSLine className="icon -left"/>Lets go back to Homepage</Link>
+      <h1>Got your Submission</h1>
+      <p>Thanks for you interest in our club. Please email us to confirm you submission.</p>
+      <Link to="/" className="button"><RiArrowLeftSLine className="icon -left"/>Back to Homepage</Link>
     </div>
 
   </Layout>
